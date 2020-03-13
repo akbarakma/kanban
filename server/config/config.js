b76b185c608dd1a9991dc2762eaad3ddc7bd5d0a
akbarakma <akbarajo1234@gmail.com>
2020-03-13 18:11:36 +0700

    merubah config js

--- server/config/config.js
+++ server/config/config.js
@@ -16,11 +16,10 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "username": "usfvsmpyopanhg",
+    "password": "7fd4e4963e85157f4e18580cdb926b5c77d1caaf183ee5dfb77bb3f32d796785",
+    "database": "dfk87v75vksh30",
+    "host": "ec2-54-152-175-141.compute-1.amazonaws.com",
+    "dialect": "postgres"
   }
 }
